a0dfa61be0ff1f0a2b6dcf99f9e8142eac68ee51
Mateo-Castro06 <mateoruedacastro11@gmail.com>
2026-07-18 19:51:11 -0500

    defined color palette

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import "../styles/global.css";
+import "../styles/home.css";
+import "../styles/colors.css"
+
 function Home(){
     return (
         <main>
             <h1>GymManager</h1>
             <p>Manage your gym quickly and efficiently</p>
             <button>Login</button>
+            <button>Register</button>
         </main>
     )
 }
