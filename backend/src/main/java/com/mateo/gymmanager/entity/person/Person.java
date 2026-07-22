12b7c3db3fa7b89e2f0113c93853749d391fcb52
Mateo-Castro06 <mateoruedacastro11@gmail.com>
2026-07-22 11:13:30 -0500

    implementación clase Person.java

--- backend/src/main/java/com/mateo/gymmanager/entity/person/Person.java
+++ backend/src/main/java/com/mateo/gymmanager/entity/person/Person.java
@@ -1,11 +1,35 @@
 package com.mateo.gymmanager.entity.person;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "person")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long idPerson;
+
+    @Column(nullable = false)
+    protected String firstName;
+
+    protected String middleName;
+
+    @Column(nullable = false)
+    protected String lastName;
+
+    @Column(nullable = false)
+    protected String dni;
+
+    @Column(nullable = false)
+    protected LocalDate birthDate;
 
 }
