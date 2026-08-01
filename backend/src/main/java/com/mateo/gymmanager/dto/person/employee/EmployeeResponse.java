16c13e4033cd688b3e5010f20189cd1bbf614089
Mateo-Castro06 <mateoruedacastro11@gmail.com>
2026-08-01 14:36:08 -0500

    labels lombok

--- backend/src/main/java/com/mateo/gymmanager/dto/person/employee/EmployeeResponse.java
+++ backend/src/main/java/com/mateo/gymmanager/dto/person/employee/EmployeeResponse.java
@@ -1,8 +1,17 @@
 package com.mateo.gymmanager.dto.person.employee;
 
 import com.mateo.gymmanager.entity.person.employee.EmployeeRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class EmployeeResponse {
     private Long employeeId;
